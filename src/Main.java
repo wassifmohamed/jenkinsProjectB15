71796c054cb28646ecaa96258387a09593e73a41
wassifmohamed <wassifmohamed@gmail.com>
2023-10-20 21:05:30 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+
+        for (int i=0; i<10;i++){
+            System.out.println(" hello the world I'm Coming ");
+            Thread.sleep(2000);
+
+        }
+
+
+    }
+}
