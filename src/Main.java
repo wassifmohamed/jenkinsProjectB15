1aeb5f653dcb38761b66a4de1a3c9716a613e957
wassifmohamed <wassifmohamed@gmail.com>
2023-10-22 08:53:42 -0400

    switch added

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,19 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
 
+
+        int day = 4;
+        switch (day) {
+            case 6:
+                System.out.println("Today is Saturday");
+                break;
+            case 7:
+                System.out.println("Today is Sunday");
+                break;
+            default:
+                System.out.println("Looking forward to the Weekend");
+        }
+
         for (int i=0; i<20;i++){
             System.out.println(" hello the world I'm Coming ");
             Thread.sleep(1000);
